13aa70d0946d5aff3b364b241b8f4c83c5bb820f
huangxinglong <414751667@example.com>
2019-02-15 11:57:29 +0800

    渲染

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="login-wrap">
     <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-        <h2>用户登陆</h2>
+      <h2>用户登陆</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">主要按钮</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登陆</el-button>
     </el-form>
   </div>
 </template>
@@ -21,6 +21,36 @@ export default {
         username: '',
         password: ''
       }
+    }
+  },
+  methods: {
+    // 登陆请求
+    handleLogin () {
+      // 前提：api-server -> app.js
+      this.$http
+        .post(`login`, this.formdata)
+        .then(res => {
+          console.log(res)
+          //   const {
+          //     data: {
+          //       data,
+          //       meta: { msg, status }
+          //     }
+          //   } = res
+          const status = res.data.meta.status
+          if (status === 200) {
+            // 渲染home.vue
+            this.$router.push({
+              name: 'home'
+            })
+          } else {
+            // 提示框 UI
+            this.$message.error('密码错误')
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   }
 }
